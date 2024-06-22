1bb59877e110df35108ee37700821f5958d14f5e
darshverma <darshverma@gmail.com>
2024-06-22 14:44:35 -0700

    Update mic.js

--- frontend/src/components/mic.js
+++ frontend/src/components/mic.js
@@ -19,6 +19,7 @@ export default function Dictaphone(){
         onRecordingComplete={(blob) => addAudioElement(blob)}
         recorderControls={recorderControls}
         downloadOnSavePress={true}
+        downloadFileExtension="mp3"
       />
       <button onClick={recorderControls.stopRecording}>Stop recording</button>
     </div>
